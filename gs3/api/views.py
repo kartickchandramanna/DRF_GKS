a615873cb9aeb4c0fd1ad9c66098ff82247959b7
Kartick Chandra Manna <kartickmanna102@gmail.com>
2023-01-02 22:48:23 +0530

    continue gs3

---
+++ gs3/api/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from .models import *
+from .serializers import *
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+
+# Create your views here.
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        
+        stream = io.ByteIO(json_data)    # converting json data into stream
+        python_data = JSONParser().parse(stream) 
+        id = python_data.get('id',None)
+        if id is not None:
+            stu = Student.objects.get(id = id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+
+            return HttpResponse(json_data, content_type = 'application/json')
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many = True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type = 'application/json')
+
